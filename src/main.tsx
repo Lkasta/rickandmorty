2b7e487c14e1be28ce09852c9fbd8bdd79b1a34b
Luiz Karpinski <lkastabackup@gmail.com>
2024-10-27 10:58:09 -0300

    create project init

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from './components/theme-provider.tsx'
+import ErrorPage from './routes/errorPage.tsx';
+import Characters from './routes/Characters/Characters.tsx';
+import Home from './routes/Home.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/characters",
+        element: <Characters />
+      },
+      {
+        path: "/home",
+        element: <Home />
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 )
---
+++ src/routes/Characters/Characters.tsx
@@ -0,0 +1,7 @@
+export default function Characters() {
+  return (
+    <div className="">
+      Personageinz
+    </div>
+  )
+}
